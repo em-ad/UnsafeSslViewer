5801669c92177287e872212545368c4b758c0e4b
Emad M <emad.mollaei@gmail.com>
2021-12-13 11:26:11 +0330

    ui

--- app/src/main/java/com/emad/didebanviewer/MainActivity.kt
+++ app/src/main/java/com/emad/didebanviewer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.emad.didebanviewer
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.tvPrepare.setOnClickListener {
